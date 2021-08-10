4f4dc563c08762d53a6bf7640aefc9f28de3a076
eshwarenm <eshwarenm@gmail.com>
2021-08-10 19:23:01 +0800

    Initial commit
    🥳

---
+++ pages/api/users.js
@@ -0,0 +1,30 @@
+import dbConnect from '../../lib/dbConnect'
+import User from '../../models/User'
+
+export default async function handler (req, res) {
+  const { method } = req
+
+  await dbConnect()
+
+  switch (method) {
+    case 'GET':
+      try {
+        const users = await User.find({})
+        res.status(200).json({ success: true, data: users })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+    case 'POST':
+      try {
+        const user = await User.create(req.body)
+        res.status(201).json({ success: true, data: user })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+    default:
+      res.status(400).json({ success: false })
+      break
+  }
+}
